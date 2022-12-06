1df8c4c77936557be51a086f818031a3516a9f95
Evan Lin <evan.if.lin@linecorp.com>
2022-12-06 16:40:53 +0800

    Init repo

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kkdai/chatgpt
+
+go 1.19
+
+require github.com/mrjones/oauth v0.0.0-20190623134757-126b35219450
---
+++ response_struct.go
@@ -0,0 +1,19 @@
+package gptchat
+
+type Completion struct {
+	ID      string `json:"id"`
+	Object  string `json:"object"`
+	Created int    `json:"created"`
+	Model   string `json:"model"`
+	Choices []struct {
+		Text         string      `json:"text"`
+		Index        int         `json:"index"`
+		Logprobs     interface{} `json:"logprobs"`
+		FinishReason string      `json:"finish_reason"`
+	} `json:"choices"`
+	Usage struct {
+		PromptTokens     int `json:"prompt_tokens"`
+		CompletionTokens int `json:"completion_tokens"`
+		TotalTokens      int `json:"total_tokens"`
+	} `json:"usage"`
+}
